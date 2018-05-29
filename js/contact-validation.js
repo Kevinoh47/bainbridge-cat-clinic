56b02a8c14abd251d776359133a23b026f295ecd
Kevinoh47 <kohok47@gmail.com>
2018-05-29 13:23:12 -0700

    small refactor contact validation

--- js/contact-validation.js
+++ js/contact-validation.js
@@ -1,10 +1,24 @@
-function checkUserName() {
+// function checkUserName() {
+//   var elMsg=document.getElementById('username-feedback');
+//   if (this.value.length < 5) {
+//     elMsg.textContent = 'The name field requires 5 or more characters.';
+//   } else {
+//     elMsg.textContent = '';
+//   }
+// }
+// var elUsername = document.getElementById('username');
+// elUsername.addEventListener("blur", checkUserName, false);
+
+function checkUserName(e, minLength) {
+  var target = e.target;
   var elMsg=document.getElementById('username-feedback');
-  if (this.value.length < 5) {
-    elMsg.textContent = 'The name field requires 5 or more characters.';
+  if (target.value.length < minLength) {
+    elMsg.textContent = 'The name field requires ' + minLength + ' or more characters.';
   } else {
     elMsg.textContent = '';
   }
 }
 var elUsername = document.getElementById('username');
-elUsername.addEventListener("blur", checkUserName, false);
+elUsername.addEventListener("blur", function(e) {
+  checkUserName(e, 4);
+}, false);
