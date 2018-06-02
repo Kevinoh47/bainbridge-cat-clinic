cf4f0c63ee2bf2c7de1cb44d8576251273651175
Kevinoh47 <kohok47@gmail.com>
2018-06-02 13:29:46 -0700

    buggy but committing for now

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -1,11 +1,14 @@
 var items = [];
 var prices = [];
+var productMap = new Map();
 
-function Cart (items, prices) {
+function Cart (items, prices, prodMap) {
   this.items = items;
   this.prices = prices;
+  this.prodMap = prodMap;
+  
   var subtotal = 0;
-
+  
   this.getSubtotal = function() {
 
     var i, len = prices.length;
@@ -21,7 +24,7 @@ function Cart (items, prices) {
 
 var elCart = document.getElementById('shopping-cart');
 
-function addToCart(e) {
+function addToLists(e) {
   e.preventDefault();
   var target = e.target;
   var itemSKU = target.id;
@@ -31,17 +34,84 @@ function addToCart(e) {
 
   items.push(itemName);
   prices.push(itemPrice);
+  productMap.set(itemName, itemPrice);
+}
+
+function formatCart(Cart) {
+  var myCart = Cart;
+  var myItems = myCart.items.slice(); //clone array to avoid using same reference.
+  var myPrices = myCart.prices.slice();
+  var mySubTotals = myCart.getSubtotal();
+
+  // console.log("items - global:", items);
+  // console.log("prices - global:", prices);
+  // console.log("myItems:", myItems);
+  // console.log("myPrices:", myPrices);
+  // console.log("mySubtotals:", mySubTotals);
+
+  var myCartList = document.getElementById('shopping-cart-list');
+
+  //build item list
+  var i=0, itemsLen = myItems.length, j=0, pricesLen = myPrices.length;
+  var newLi, currentItem,currentPrice,newText;
+
+  for (i; i < itemsLen; i++) {
+    if (i in myItems) {
+      // unfortunately, in some cases, this is looping twice where it should only loop once...
+      for (j; j < pricesLen; j++) {
+        if (j in myPrices) {
+          newLi = document.createElement('li');
+          currentItem = myItems.pop();
+          currentPrice = myPrices.pop();
+          newText = document.createTextNode(currentItem + ': ' + currentPrice)
+          newLi.appendChild(newText);
+          myCartList.appendChild(newLi);
+        }
+      }
+    }
+  }
+
+
+  //console.log("productMap", productMap);
+
+  
+  // build subtotal display
+  // var mySubtotalDisplay = document.getElementById('shopping-cart');
+  // var newDiv = document.createElement('div');
+  // var newSubAmountNode = document.createTextNode('$ ' + mySubTotals);
+  // newDiv.appendChild(newSubAmountNode);
+
+  // if (!document.getElementById("cart-subtotal")) {
+  //   // get parent node
+  //   var mySubtotalDisplay = document.getElementById('shopping-cart');
+  //   //build new div
+  //   var newDiv = document.createElement('div');
+  //   var newAttId = document.createAttribute("id");       
+  //   newAttId.value = "cart-subtotal";                           
+  //   newDiv.setAttributeNode(newAttId); 
+  //   //create sub amount
+  //   var newSubAmountNode = document.createTextNode('$ ' + mySubTotals);
+  //   // append sub amount to new div
+  //   newDiv.appendChild(newSubAmountNode);
+  //   //append new div to parent node
+  //   mySubtotalDisplay.appendChild(newDiv);
+  // }
+  // else {
+  //   console.log('yay...')
+  //   var mySubtotalDiv = document.getElementById("cart-subtotal");
+  //   //newDiv.innerHTML=newSubAmountNode; //TODO not sure this works.
+  // }
 }
 
 function displayCart(e) {
-  addToCart(e);
+  addToLists(e);
 
   var myCart = new Cart(items, prices);
-  var mySubtotal = myCart.getSubtotal();
-  var elCart = document.getElementById('shopping-cart');
+  // var mySubtotal = myCart.getSubtotal();
+  // var elCart = document.getElementById('shopping-cart');
+  // elCart.innerHTML = myCart.items;
 
-  //TODO: replace with a call to a function that formats a list
-  elCart.innerHTML = myCart.items;
+  formatCart(myCart);
 }
 
 var el = document.getElementById('product-tables');
