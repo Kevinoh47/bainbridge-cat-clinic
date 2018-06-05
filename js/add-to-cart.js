7890b8073630bb7c1e320b37aecfb7311fdc5b6f
Kevinoh47 <kohok47@gmail.com>
2018-06-05 12:51:53 -0700

    now passes current eslint config

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -2,30 +2,26 @@ var items = [];
 var prices = [];
 var productMap = new Map();
 
-function Cart (items, prices, prodMap) {
-  this.items = items;
-  this.prices = prices;
-  this.prodMap = prodMap;
-  
+function Cart(myItems, myPrices, myProdMap) {
+  this.items = myItems;
+  this.prices = myPrices;
+  this.prodMap = myProdMap;
   var subtotal = 0;
-  
-  this.getSubtotal = function() {
-
-    var i, len = prices.length;
-    for (i=0; i<len; ++i) {
+  this.getSubtotal = function getSubtotal() {
+    var len = prices.length;
+    for (var i = 0; i < len; i++) {
       if (i in prices) {
-        //regex courtesy of http://www.josscrowcroft.com/2011/code/format-unformat-money-currency-javascript/
+        //  regex courtesy of http://www.josscrowcroft.com/2011/code/format-unformat-money-currency-javascript/
         subtotal += parseFloat(prices[i].replace(/[^0-9-.]/g, ''));
       }
     }
     return subtotal.toFixed(2);
-  }
+  };
 }
 
 function addToLists(e) {
   e.preventDefault();
   var target = e.target;
-  var itemSKU = target.id;
   var grandparent = target.parentNode.parentNode;
   var itemName = grandparent.getElementsByClassName('prod-name')[0].innerText;
   var itemPrice = grandparent.getElementsByClassName('prod-price')[0].innerText;
@@ -35,47 +31,45 @@ function addToLists(e) {
   productMap.set(itemName, itemPrice);
 }
 
-function formatCart(Cart) {
-  var myCart = Cart;
-  var myItems = myCart.items.slice(); //clone array to avoid using same reference.
-  var myPrices = myCart.prices.slice();
-  var mySubTotals = myCart.getSubtotal();
-  var newSubAmountNode = document.createTextNode('Subtotal: $ ' + mySubTotals);
+function formatCart(cart) {
+  var myCart = cart;
+  var mySubTotals = 'Subtotal: $ ' + myCart.getSubtotal();
+  var newSubAmountNode = document.createTextNode(mySubTotals);
   var myProdMap = myCart.prodMap;
 
   var myCartList = document.getElementById('shopping-cart-list');
   // remove current children
   while (myCartList.firstChild) {
-    var removeEl = myCartList.firstChild;
-    myCartList.removeChild(removeEl);
+    var removeElement = myCartList.firstChild;
+    myCartList.removeChild(removeElement);
   }
-  //build/rebuild item list
-  var i=0, itemsLen = myItems.length, j=0, pricesLen = myPrices.length;
-  var newLi, currentItem,currentPrice,newText;
+
+  //  build/rebuild item list
+  var newLi;
+  var newText;
 
   for (var entry of myProdMap.entries()) {
     newLi = document.createElement('li');
     newText = document.createTextNode(entry[0] + ': ' + entry[1]);
     newLi.appendChild(newText);
     myCartList.appendChild(newLi);
-  }  
-  
+  }
+
   // build subtotal display
-  if (!document.getElementById("cart-subtotal")) {
-    // get parent node
+  if (!document.getElementById('cart-subtotal')) {
+    //  get parent node
     var mySubtotalDisplay = document.getElementById('shopping-cart');
-    //build new div
+    //  build new div
     var newDiv = document.createElement('div');
-    var newAttId = document.createAttribute("id");       
-    newAttId.value = "cart-subtotal";                           
-    newDiv.setAttributeNode(newAttId); 
+    var newAttId = document.createAttribute('id');
+    newAttId.value = 'cart-subtotal';
+    newDiv.setAttributeNode(newAttId);
     // append sub amount to new div
     newDiv.appendChild(newSubAmountNode);
-    //append new div to parent node
+    //  append new div to parent node
     mySubtotalDisplay.appendChild(newDiv);
-  }
-  else {
-    var mySubtotalDiv = document.getElementById("cart-subtotal");
+  } else {
+    var mySubtotalDiv = document.getElementById('cart-subtotal');
     // remove current children
     while (mySubtotalDiv.firstChild) {
       var removeEl = mySubtotalDiv.firstChild;
@@ -91,9 +85,9 @@ function displayCart(e) {
   formatCart(myCart);
 }
 
-//event listener delegates from the table container.
+//  event listener delegates from the table container.
 var el = document.getElementById('product-tables');
-el.addEventListener('click', function(e) {
+el.addEventListener('click', function (e) {
   displayCart(e);
 }, false);
 
--- js/display-tables.js
+++ js/display-tables.js
@@ -1,10 +1,10 @@
 // Courtesy of https://stackoverflow.com/questions/14972708/how-to-get-all-selected-option-text-form-multi-select-using-javascript
 function GetSelectValues(select) {
-  var result = [];
-  var options = select && select.options;
+  const result = [];
+  const options = select && select.options;
   var opt;
 
-  for (var i=0, iLen=options.length; i<iLen; i++) {
+  for (var i = 0, iLen = options.length; i < iLen; i++) {
     opt = options[i];
 
     if (opt.selected) {
@@ -15,19 +15,19 @@ function GetSelectValues(select) {
 }
 
 function displayHiddenElements(e) {
-  e.preventDefault();
-  var elMultiSelect=document.getElementById('product-types');
+  const elMultiSelect = document.getElementById('product-types');
   var selected = GetSelectValues(elMultiSelect);
-  
-  for (i=0; i < selected.length; i++) {
+
+  e.preventDefault();
+  for (let i = 0; i < selected.length; i++) {
     var currParDiv = document.getElementById(selected[i]).parentElement;
     var currClass = currParDiv.getAttribute('class');
 
-    if (currParDiv.hasAttribute('class') && currClass == 'hidden') {
+    if (currParDiv.hasAttribute('class') && currClass === 'hidden') {
       currParDiv.setAttribute('class', 'visible');
     }
   }
 }
 var elDisplayProds = document.getElementById('show-products-submit');
-elDisplayProds.addEventListener("click", displayHiddenElements, false);
+elDisplayProds.addEventListener('click', displayHiddenElements, false);
 
